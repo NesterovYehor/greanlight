26207113dfb71166eb5342e94bd5797217f75424
Yehor Nesterov <nesterovyehorgithub@gmail.com>
2024-10-10 11:54:29 +0200

    First PSQL Migration

---
+++ migrations/000001_create_movies_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS movies(
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW(),
+    title text NOT NULL,
+    year integer NOT NULL,
+    genres  text[] NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
